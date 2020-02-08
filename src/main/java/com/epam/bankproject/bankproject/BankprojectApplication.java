f723a8a84089f95f74f7222dd76ed7fd29865d9f
nikitakyrpenko@gmail.com <nikitakyrpenko@gmail.com>
2020-02-08 15:45:35 +0200

    init project repository

---
+++ src/main/java/com/epam/bankproject/bankproject/BankprojectApplication.java
@@ -0,0 +1,13 @@
+package com.epam.bankproject.bankproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/bankproject/bankproject/BankprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.bankproject.bankproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
