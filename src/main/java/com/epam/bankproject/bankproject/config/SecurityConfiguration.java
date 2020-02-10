59b49228cc80261825bd808e52171a7000f10d19
nikitakyrpenko@gmail.com <nikitakyrpenko@gmail.com>
2020-02-10 11:56:53 +0200

    Password Encoder (fix) Mockito dependency

---
+++ src/main/java/com/epam/bankproject/bankproject/config/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.epam.bankproject.bankproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/epam/bankproject/bankproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/epam/bankproject/bankproject/service/impl/UserServiceImpl.java
@@ -7,19 +7,17 @@ import com.epam.bankproject.bankproject.service.UserService;
 import com.epam.bankproject.bankproject.service.exception.DuplicateEntityException;
 import com.epam.bankproject.bankproject.service.mapper.Mapper;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor(onConstructor = @__(@Autowired))
-@NoArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
-    //private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private PasswordEncoder passwordEncoder;
     private Mapper<User, UserEntity> userMapper;
 
 
@@ -34,10 +32,9 @@ public class UserServiceImpl implements UserService {
     public void register(@NotNull User user) {
         userRepository.findById(user.getId())
                 .ifPresent(userEntity -> {throw new DuplicateEntityException("User already exist");});
-
-        /*User userWithHashedPassword = user.toBuilder()
-                .password(bCryptPasswordEncoder.encode(user.getPassword()))
+        User userWithHashedPassword = user.toBuilder()
+                .password(passwordEncoder.encode(user.getPassword()))
                 .build();
-        userRepository.save(userMapper.mapDomainToEntity(userWithHashedPassword));*/
+        userRepository.save(userMapper.mapDomainToEntity(userWithHashedPassword));
     }
 }
